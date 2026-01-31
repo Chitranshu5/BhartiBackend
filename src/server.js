2577f9a358d8c2f0062621571ef3e7718115fc75
Chitranshu-7 <chitranshuctr@gmail.com>
2026-02-01 04:18:05 +0530

    done

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
-import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,38 +10,32 @@ import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
 
-// hell
-
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" }));
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
-app.options("*", cors({ origin: true, credentials: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://prismatic-axolotl-69652b.netlify.app",
+    credentials: true, // only if you use cookies
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
-  try {
-    res.status(200).json({ message: "Server is running 🚀" });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong", error });
-  }
+  res.status(200).json({ message: "Server is running 🚀" });
 });
 
-
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
