b67fa9075c6305972124b6c6a67d882bf9536cc0
Chitranshu-7 <chitranshuctr@gmail.com>
2026-02-04 21:16:38 +0530

    done

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -8,7 +7,6 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
